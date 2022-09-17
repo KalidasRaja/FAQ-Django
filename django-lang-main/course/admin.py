049572814e85631cbb1cf22049409a5d2a014710
Kalidas <k.s.kalidasraja@gmail.com>
2022-09-17 19:55:15 +0530

    Removed course App

--- django-lang-main/course/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from parler.admin import TranslatableAdmin
-
-from .models import Category, Faq_QA
-
-admin.site.register(Category, TranslatableAdmin)
-admin.site.register(Faq_QA, TranslatableAdmin)
--- django-lang-main/course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-# from .views import ListCourse, detail
-#
-# urlpatterns = [
-#     path('', ListCourse.as_view()),
-#     path('detail/', detail, name="detail")
-# ]
